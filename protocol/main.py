c6cd778a5f39c0bc9aec3223e456ff0d4c495c80
Ryan Vacek <ryanvacek@Ryans-MacBook-Pro.local>
2023-02-21 13:12:43 -0700

    adds testnet to genesis report and adds the report

--- protocol/main.py
+++ protocol/main.py
@@ -10,8 +10,11 @@ import urllib.request
 import sys
 import argparse
 
+DATA_REGISTRY=os.getcwd() + "/data"
+
 CHAIN_REGISTRY_URI='https://github.com/cosmos/chain-registry'
 CHAIN_REGISTRY_DIR=os.getcwd() + "/data/chain-registry-cosmos"
+CHAIN_TESTNET_REGISTRY_DIR=os.getcwd() + "/data/chain-registry-cosmos-testnet"
 CHAIN_GENESIS_DIR=os.getcwd() + "/data/genesis/"
 
 DATA_EXPORT=os.getcwd() + "/data/export"
@@ -44,7 +47,7 @@ def zip_data_folder(input_folder, output_folder):
 class ChainManager():
     def __init__(self, file_path):
         self.file_path = file_path
-        self.chains_list= []
+        self.chains_list = []
         self.genesis_grab_report = {}
         self.genesis_data = {}
         self.genesis_data["name"] = []
@@ -55,6 +58,7 @@ class ChainManager():
         self.genesis_data_report["uri"] = []
         self.genesis_data_report["success"] = []
         self.genesis_data_report["error"] = []
+        self.chains_list_testnet = []
         try:
             subprocess.run(['git', 'clone', CHAIN_REGISTRY_URI, CHAIN_REGISTRY_DIR])
             self.clean_directory()
@@ -62,34 +66,64 @@ class ChainManager():
         except Exception as e:
             logging.error(str(e))
     def clean_directory(self):
-            print("cleaning registry directory")
-            remove_dirs=["/.git", "/.github", "/_IBC", "/_non-cosmos", "/testnets"]
-            remove_files=["/.gitignore", "README.md", "/chain.schema.json", "/ibc_data.schema.json", "/README.md", "/assetlist.schema.json"]
-            for remove_dir in remove_dirs:
-                try:
-                    shutil.rmtree(CHAIN_REGISTRY_DIR+remove_dir)
-                except OSError as e:
-                    continue
+            logging.info("Cleaning registry directory")
+            remove_dirs=["/.git", "/.github", "/_IBC", "/_non-cosmos"]
+            remove_dirs_testnet=["/_IBC", "/_non-cosmos"]
+            remove_files=["/.gitignore", "/README.md", "/chain.schema.json", "/ibc_data.schema.json", "/assetlist.schema.json"]
+            shutil.move(CHAIN_REGISTRY_DIR+"/testnets", DATA_REGISTRY)
+            os.rename(DATA_REGISTRY+"/testnets", CHAIN_TESTNET_REGISTRY_DIR)
+            logging.info("Testnet directory created: " + CHAIN_TESTNET_REGISTRY_DIR)
             for remove_file in remove_files:
                 try:
+                    logging.info("Removing file" + CHAIN_REGISTRY_DIR+remove_file)
                     os.remove(CHAIN_REGISTRY_DIR+remove_file)
                 except OSError as e:
+                    logging.error("Failed to remove file: " + CHAIN_REGISTRY_DIR + remove_file)
+                    continue
+                finally:
+                    logging.info("Removed file: " + CHAIN_REGISTRY_DIR + remove_file)
+                    continue
+            for directory in remove_dirs:
+                try:
+                    shutil.rmtree(CHAIN_REGISTRY_DIR+directory)
+                except OSError as e:
+                    logging.error("Failed to remove directory: " + CHAIN_REGISTRY_DIR + directory)
+                    continue
+                finally:
+                    logging.info("Removed directory: " + CHAIN_REGISTRY_DIR + directory)
+                    continue
+            for directory in remove_dirs_testnet:
+                try:
+                    shutil.rmtree(CHAIN_TESTNET_REGISTRY_DIR+directory)
+                except OSError as e:
+                    logging.error("Failed to remove directory:" + CHAIN_TESTNET_REGISRY_DIR + remove_directory)
+                    continue
+                finally:
+                    logging.info("Removed directory: " + CHAIN_TESTNET_REGISTRY_DIR + remove_file)
                     continue
     def create_cosmos_chains(self):
-        logging.info("creating cosmos chains")
+        logging.info("Creating cosmos chains")
         self.chains_list = os.listdir(CHAIN_REGISTRY_DIR)
+        self.chain_list_testnet = os.listdir(CHAIN_TESTNET_REGISTRY_DIR)
         self.chains = []
         for c in self.chains_list:
             try:
                 chain_file = CHAIN_REGISTRY_DIR+"/"+c+"/"+CHAIN_FILE
+                logging.info("Adding cosmos chain " + chain_file)
                 self.chains.append(TendermintChain(c, chain_file))
             except Exception as e:
-                logging.error(traceback.format_exc())
-                print("Failed to add chain: "+c+"\n"+"chain_file: "+chain_file+"\n")
+                logging.error("Failed to add chain: "+c+"\n"+"chain_file: "+chain_file+"\n")
+                continue
+        for c in self.chains_list_testnet:
+            try:
+                chain_file = CHAIN_TESTNET_REGISTRY_DIR+"/"+c+"/"+CHAIN_FILE
+                logging.info("Adding testnet cosmos chain " + chain_file)
+                self.chains.append(TendermintChain(c, chain_file))
+            except Exception as e:
+                logging.error("Failed to add chain: "+c+"\n"+"chain_file: "+chain_file+"\n")
                 continue
         for chain in self.chains:
-            logging.info("Adding genesis file for: " + chain)
-            chain.add_genesis(self.append_genesis_report)
+            chain.add_genesis(self.append_genesis_report, "false")
     def print_genesis_data(self):
         for chain in self.chains:
             if hasattr(chain, "genesis_url"):
@@ -115,15 +149,15 @@ class ChainManager():
         except Exception as e:
             logging.error(e)
         finally:
-            logging.info("succesfully saved genesis data")
+            logging.info("Succesfully saved genesis data")
     def export_genesis_final(self):
         try:
             df = pd.DataFrame(self.genesis_data_report, orient='index')
             df.to_csv('/data/export/genesis-all.csv')
         except Exception as e:
-            logging.error("failed to add genesis data: "+e)
+            logging.error("Failed to add genesis data: "+e)
         finally:
-            logging.info("succesfully saved genesis data")
+            logging.info("Successfully saved genesis data")
 
 class TendermintChain:
     def __init__(self, name, chain_file_path):
@@ -151,34 +185,28 @@ class TendermintChain:
                         self.fee_low_gas_price = fee_tokens[0].get("low_gas_price")
                         self.fee_average_gas_price = fee_tokens[0].get("average_gas_price")
                         self.fee_high_gas_price = fee_tokens[0].get("high_gas_price")
-
                 staking = data.get("staking")
                 if staking:
                     staking_tokens = staking.get("staking_tokens")
                     if staking_tokens:
                         self.staking_denom = staking_tokens[0].get("denom")
-
                 codebase = data.get("codebase")
                 if codebase:
                     self.git_repo = codebase.get("git_repo")
                     self.recommended_version = codebase.get("recommended_version")
                     self.compatible_versions = codebase.get("compatible_versions")
-                    
                     genesis = codebase.get("genesis")
                     if genesis:
                         self.genesis_url = genesis.get("genesis_url")
-
                 peers = data.get("peers")
                 if peers:
                     seeds = peers.get("seeds")
                     if seeds:
                         self.seed_id = seeds[0].get("id")
                         self.seed_address = seeds[0].get("address")
-                        
                     persistent_peers = peers.get("persistent_peers")
                     if persistent_peers:
                         self.persistent_peers = persistent_peers
-
                 apis = data.get("apis")
                 if apis:
                     rpc = apis.get("rpc")
@@ -190,14 +218,13 @@ class TendermintChain:
                     grpc = apis.get("grpc")
                     if grpc:
                         self.grpc_addresses = [x.get("address") for x in grpc]
-
                 explorers = data.get("explorers")
                 if explorers:
                     self.explorer_urls = [x.get("url") for x in explorers]
                     self.tx_pages = [x.get("tx_page") for x in explorers]
         except Exception as e:
                 logging.error(traceback.format_exc())
-    def add_genesis(self, append_genesis_report_func):
+    def add_genesis(self, append_genesis_report_func, testnet):
         try:
             logging.info("Downloading genesis file "+self.name+" at "+self.genesis_url)
             response = {}
@@ -216,7 +243,7 @@ class TendermintChain:
             append_genesis_report_func(self.name, self.genesis_url, "false", str(e))
             logging.error("failed to grab genesis due to error: "+str(e))
         except Exception as e:
-            append_genesis_report_func(self.name, self.genesis_url, "false", e)
+            append_genesis_report_func(self.name, self.genesis_url, "false", str(e))
             logging.error("failed to grab genesis due to error: "+str(e))
         finally:
             self.save_genesis(self.genesis_data, CHAIN_GENESIS_DIR+"/"+self.name+"genesis.json")
@@ -252,7 +279,6 @@ class Genesis:
             self.params = genesis_dict.get("params")
             self.evidence = genesis_dict.get("evidence")
             self.auth = genesis_dict.get("auth")
-
     def to_dict(self):
         return {
             "genesis_time": self.genesis_time,
@@ -272,7 +298,6 @@ class Genesis:
             "evidence": self.evidence,
             "auth": self.auth,
         }
-
     def load_dataframe(self):
         with open(genesis_path) as f:
             self.dataframe = pd.read_json(self.genesis_path)
@@ -293,6 +318,7 @@ def main():
         logging.info("Removing "+ CHAIN_REGISTRY_DIR + " and " + CHAIN_GENESIS_DIR)
         shutil.rmtree(CHAIN_REGISTRY_DIR)
         shutil.rmtree(CHAIN_GENESIS_DIR)
+        shutil.rmtree(CHAIN_TESTNET_REGISTRY_DIR)
 
     if os.path.isdir(CHAIN_GENESIS_DIR):
         print(CHAIN_GENESIS_DIR + "folder exists.")
